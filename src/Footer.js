6f4d1897fb4bd5f42dc18495b1253e1f4625ba9d
joebakid <josephbawo@gmail.com>
2024-09-26 23:03:07 +0100

    footer text adjustment

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function footer() {
   return (
     <footer className="footer">
-      <p className="center">© 2024 Dogwifmog . All Rights Reserved.</p>
+      <p className="center footer-text">© 2024 Dogwifmog . All Rights Reserved.</p>
     </footer>
   );
 }
